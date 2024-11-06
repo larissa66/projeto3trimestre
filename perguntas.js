296451465da89c4f0b51fa0df339b4fbb694ca18
larissa66 <162442359+larissa66@users.noreply.github.com>
2024-11-06 07:59:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Desenho animado',
+    'Quem é o vilão que está sempre tentando capturar o "Pernalonga" nos desenhos da warner bros?',
+    'hortelino troca-letras'
+)
+
+criaCartao(
+    'Artes',
+    'Qual obra famosa de arte de Leonardo da Vinci retrata uma mulher com um sorriso enigmático',
+    'Monalisa'
+)
+
+criaCartao(
+    'Musica',
+    'O que é uma clave na nptação musical?',
+    'A clave é um simbolo colocado no inicio de uma pauta musical que indica a altura das notas em relação.'
+)
+
+criaCartao(
+    'Desenho animado',
+    'Qual série de desenho da Nickelodeon segue a aventuras de um garoto e eu cachorro mágico em uma terra pós-apocaliptica?',
+    'Hora de aventura'
+)
